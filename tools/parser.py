002480baffa1160054ebca7530493058d2ce4ecf
fxbrit <34379516+fxbrit@users.noreply.github.com>
2023-03-16 15:43:12 +0100

    Fix #4: New Connection IDs are new connections in Spindump (#5)
    * New Connection IDs are new connections in Spindump
    
    * Add duration of the marking interval to output
    
    * Detect intervals with single packet, show seconds passed alongside ms
    
    * Produce a more readable output

--- tools/parser.py
+++ tools/parser.py
@@ -32,17 +32,20 @@ def parse_file(filename):
         spins.append(spin)
   # Each connection has its own Spin Bit value so we should divide
   # by destination IP address first, and then sort by timestamp.
-  spins.sort(key=itemgetter("dest_ipaddr", "timestamp"))
+  spins.sort(key=itemgetter("dest_ipaddr", "connection_id", "timestamp"))
   for spin in spins:
     if dest_ip != spin["dest_ipaddr"]:
       dest_ip = spin["dest_ipaddr"]
       out += "\nDestination IP address: " + str(dest_ip) + "\n"
-      current_spin = 0
-      flip_counter = 0
-      start_timestamp = None
+      current_spin, flip_counter, start_timestamp = (0,0,None)
     if conn_id != spin["connection_id"]:
       conn_id = spin["connection_id"]
-      out += "\tConnection ID: " + str(conn_id) + "\n"
+      # Spindump parses new Connection IDs only on Long Headers. For Short Headers
+      # it uses UDP, IP-IP, port-port instead: this means a new Connection ID is a new
+      # connection towards the same IP address.
+      out += "\tConnection ID: " + str(conn_id) + ", new connection towards " + str(dest_ip) + "\n"
+      # As a result we want to start counting flips from 0.
+      current_spin, flip_counter, start_timestamp = (0,0,None)
     # Spindump timestamps are in µs, we must convert to s.
     timestamp = datetime.fromtimestamp(spin["timestamp"] / (10.0**6))
     spin_bit = spin["spin_bit"]
@@ -51,23 +54,39 @@ def parse_file(filename):
         current_spin = spin_bit
         start_timestamp = timestamp
         flip_counter+=1
+        previous = timestamp
         out += "\t--- Flip number: " + str(flip_counter) + "\n"
       else:
+        if (previous <= start_timestamp):
+          out += "\t--- Single packet in this marking interval\n"
+        else:
+          interval = previous - start_timestamp
+          interval_s = interval.seconds
+          interval_ms = interval.microseconds * (10.0**-3)
+          out += ("\t--- Marking interval lasted at least:\t" +
+                  previous.strftime("%H:%M:%S.%f") +
+                  " - " +
+                  start_timestamp.strftime("%H:%M:%S.%f") +
+                  " = " +
+                  str(interval_s*1000 + interval_ms) + "ms\n")
         time_passed = timestamp - start_timestamp
-        ms = time_passed.microseconds * (10.0**-3)
+        time_passed_s = time_passed.seconds
+        time_passed_ms = time_passed.microseconds * (10.0**-3)
         # This statement can be used to debug and filter out small
         # time intervals that might represent fuzzy edges.
-        if (ms >= 0):
-          out += ("\t--- Time passed: " +
+        if (time_passed_ms >= 0):
+          out += ("\t--- Marking interval lasted at most:\t" +
                   timestamp.strftime("%H:%M:%S.%f") +
                   " - " +
                   start_timestamp.strftime("%H:%M:%S.%f") +
                   " = " +
-                  str(ms) + "ms\n")
+                  str(time_passed_s*1000 + time_passed_ms) + "ms\n")
           current_spin = spin_bit
           start_timestamp = timestamp
           flip_counter+=1
           out += "\t--- Flip number: " + str(flip_counter) + "\n"
+    else:
+      previous = timestamp
     out += ("\t" + timestamp.strftime("%H:%M:%S.%f") + 
             " Spin Bit: " + str(spin["spin_bit"]) + "\n")
   return out
