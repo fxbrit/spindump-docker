13c9366e6476da2ade7042206a8b231644b805c6
fxbrit <34379516+fxbrit@users.noreply.github.com>
2023-03-30 16:31:44 +0200

    Add the tester tool

--- tools/Readme.md
+++ tools/Readme.md
@@ -7,3 +7,7 @@ A collection of tools to support [Spindump](https://github.com/EricssonResearch/
 Given a capture from the spindump-docker container, it produces a readable output with a summary of the Spin Bit flips. For example:
 
 <img src="./img/parser.png" width=500>
+
+## Tester
+
+Bash scripts that automates QUIC client calls to a given URL and produces both a log file and a set of Spindump captures.
---
+++ tools/tester.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+compose=name # Name of the Docker Compose config.
+spindump_path=path/to/spindump-docker # The parent directory of tools.
+chromium_path=path/to/chromium/src # The path to your Chromium checkout.
+checkout=name # The name of your checkout containing QUIC client.
+delay=100 # Delay introduced server side.
+url=https://example.local # The URL to be fetched by QUIC client.
+repeat_per_capture=1 # Number of requests for each QUIC client fetch.
+tot_captures=1 # Number of captures in this test.
+
+# Start the Docker container for the capture and create output dir.
+start_capture () {
+  cd $spindump_path
+  mkdir -p tools/tester_output_$delay
+  docker compose up -d $compose
+}
+
+# Stop the capture and move the capture file:
+#   $1 is capture number.
+stop_capture_and_move_output () {
+    cd $spindump_path
+    docker compose stop $compose
+    mv $compose/capture.json tools/tester_output/capture_$1.json
+}
+
+# Execute a fetch with QUIC client:
+#   $1 is capture number.
+quic_go_get_it () {
+    cd $quic_client_path
+    ./out/$checkout/quic_client $url --quiet --num_requests=$repeat_per_capture > $spindump_path/log_$1.txt 2>&1
+}
+
+for i  in {1..$tot_captures};
+do
+    echo 'Starting capture number $i...'
+    start_capture
+    echo 'QUIC client doing its thing...'
+    quic_go_get_it $i
+    echo 'Stopping and saving the capture...'
+    stop_capture_and_move_output $i
+    echo 'Finished capture number $i.' 
+done
